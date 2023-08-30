aa20a57a2735ea0b2b59681796c7266702a9b105
FusaishiHaruaki <100342771+FusaishiHaruaki@users.noreply.github.com>
2023-08-30 14:42:01 +1200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
 //Blinky
+//Excercise 1
 #include "mbed.h"
 
-DigitalOut led(LED1);
+DigitalIn input(BUTTON1);
+DigitalOut output(LED1);
 
 int main() {
     while (1) {
-        led = !led;
-        printf("Blink! LED is now %d\n", led.read());
+
+        
+        int x = input;
+        output = x;
+        printf("Blink! button is now %d, LED is now %d\n", input.read(), output.read());
+
 
         wait_ms(500);
+    }
+}
+
+
+//Excercise 2
+AnalogIn input(p15);
+DigitalOut output(LED1);
+int main() {
+    while (1) {
+        
+        //float x = input;
+        output = 1;
+        printf("Blink! LED is on for %f seconds\n", input.read());
+        wait_ms(input);
+        output=0;
+        printf("LED is off\n");
+        wait_ms(2000);
     }
 }
