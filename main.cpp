b681709b9669f64ec3bc75e0cde7cb5e9fa21440
FusaishiHaruaki <100342771+FusaishiHaruaki@users.noreply.github.com>
2023-09-01 19:05:25 +1200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,12 +25,21 @@ DigitalOut output(LED1);
 int main() {
     while (1) {
         
-        //float x = input;
+        float time = input;
         output = 1;
-        printf("Blink! LED is on for %f seconds\n", input.read());
-        wait_ms(input);
+        while (time > 0){
+            wait_ms(100);
+            time = time - 0.1;
+            
+        }
+        output = 0;
+        //float x = input;
+        
+        /*printf("Blink! LED is on for %f seconds\n", input.read()*2);
+        wait_ms(input*2);
         output=0;
         printf("LED is off\n");
-        wait_ms(2000);
+        */
+        wait_ms(2000-2*input);
     }
 }
